914372bdbea23b8f09248579933512e5e4bcb76a
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2023-07-26 20:20:07 -0300

    printando os tipos no terminal

--- variaveis/tipos.js
+++ variaveis/tipos.js
@@ -1,23 +1,31 @@
 // Number: Representa números inteiros ou de ponto flutuante. Por exemplo, 10, 3.14.
 let age = 30;
+console.log(age, typeof age); // typeof imprime o tipo da variável
 let pi = 3.14;
+console.log(pi, typeof pi);
 
 // String: Representa sequências de caracteres. Por exemplo, "Olá, mundo!", 'JavaScript'.
 let name = "John";
+console.log(name, typeof name);
 let message = 'Hello, world!';
+console.log(message, typeof message);
 
 // Boolean: Representa valores verdadeiros ou falsos. São true ou false.
 let isTrue = true;
 let isFalse = false;
+console.log(isTrue, typeof isTrue);
 
 // Null: Representa um valor nulo, ou seja, intencionalmente vazio ou sem valor.
 let emptyValue = null;
+console.log(emptyValue, typeof emptyValue);
 
 // Undefined: Representa uma variável que foi declarada, mas ainda não recebeu um valor.
 let notDefined;
+console.log(notDefined, typeof notDefined);
 
 // Symbol: Representa um valor único e imutável usado como chave de uma propriedade de objeto.
 const id = Symbol();
+console.log(id, typeof id);
 
 // Object: Representa uma coleção de pares chave-valor e é usado para armazenar dados mais complexos. 
 // Os objetos em JavaScript podem conter propriedades e métodos.
@@ -26,3 +34,4 @@ let person = {
   age: 25,
   isStudent: true
 };
+console.log(person, typeof person);
