61bdcdc20b41c5f574a70c24a8a12dc31bf8856c
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2023-07-27 11:37:57 -0300

    exemplo simples de integração de html e js

---
+++ frontend/index.js
@@ -0,0 +1,8 @@
+// Selecionar o botão pelo ID
+const btnSaudacao = document.getElementById('btnSaudacao');
+
+// Adicionar um ouvinte de evento de clique no botão
+btnSaudacao.addEventListener('click', function() {
+  // Exibir a mensagem de saudação no console do navegador
+  alert('Olá! Bem-vindo ao exemplo de integração HTML e JavaScript.');
+});
