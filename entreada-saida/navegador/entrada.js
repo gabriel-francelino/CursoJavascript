a918ac806569573b6db9084fe02cfbb39211d7c7
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2023-09-26 14:51:04 -0300

    entreda de dados pelo navegador

---
+++ entreada-saida/navegador/entrada.js
@@ -0,0 +1,10 @@
+/* para realizar uma leitura de dados pelo navegador é necessário o javascript estar linkado
+    com alguma página html 
+*/
+
+//leitura de dados
+// O método prompt() faz com que uma mensagem seja exibida para o usuário e o processamento do software pare, aguardando uma resposta do usuário.
+let nome = prompt('Digite seu nome:')
+
+console.log(nome)
+
